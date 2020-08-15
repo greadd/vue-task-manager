6eff96102a16e67382c4ea2037460818dfe39fa9
Gleb Yakovlev <gleb.grd@gmail.com>
2020-08-15 16:45:09 +0300

    initial commit

---
+++ dist/precache-manifest.e082439a6e5de8da0af7bbb6a82405c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc363b818a698e6677f",
+    "url": "/css/app.747616b9.css"
+  },
+  {
+    "revision": "81053d0d0e87a65ea1f0",
+    "url": "/css/chunk-34456752.ceb3e085.css"
+  },
+  {
+    "revision": "6cd401d07f160024c6d7",
+    "url": "/css/chunk-vendors.3e4956bb.css"
+  },
+  {
+    "revision": "0feff6b5d530e45c919e1de09a117d76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dc363b818a698e6677f",
+    "url": "/js/app.089df0eb.js"
+  },
+  {
+    "revision": "f802f960eb68b82301ba",
+    "url": "/js/chunk-2d0b64c8.c232864c.js"
+  },
+  {
+    "revision": "10630225f1f97c8d09de",
+    "url": "/js/chunk-2d21ef2c.faa27c75.js"
+  },
+  {
+    "revision": "81053d0d0e87a65ea1f0",
+    "url": "/js/chunk-34456752.76eec5ca.js"
+  },
+  {
+    "revision": "6cd401d07f160024c6d7",
+    "url": "/js/chunk-vendors.7a27572c.js"
+  },
+  {
+    "revision": "20e5c2643ed929075b92f6e80c282815",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
